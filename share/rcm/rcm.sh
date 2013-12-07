e58242ce67f28411ebace3a615ac01eb8760cdac
patrick brisbin <pbrisbin@gmail.com>
2013-12-07 13:18:26 -0500

    Comments, renames, minor fixes

--- share/rcm/rcm.sh
+++ share/rcm/rcm.sh
@@ -1,16 +1,24 @@
 # RCM library script
 #
-# Portability notes, based on http://apenwarr.ca/log/?m=201102#28.
+# Portability notes:
 #
-# * local is OK, thank god
-# * we can use ${var%.*} style splitting, should replace some
+# * The local keyword is OK.
+#
+# * We can use ${var%.*} style splitting. TODO: replace some
 #   sed/dirname/basename uses for performance.
-# * Assigning $@ doesn't work consistently, which means handling file
-#   arguments is difficult. For now, all array-like options are stored
-#   as space-separated strings which means the elements cannot contain
-#   spaces either.
+#
+# * Assigning $@ doesn't work consistently. For now, all array-like
+#   options are treated as space-separated strings which means the
+#   elements cannot contain spaces either.
+#
+# source: http://apenwarr.ca/log/?m=201102#28.
 #
 ###
+_cp()    { echo "cp $*"; }    # TODO
+_ln()    { echo "ln $*"; }    # TODO
+_mkdir() { echo "mkdir $*"; } # TODO
+
+# Print $* on STDERR if verbosity is high enough.
 debug() {
   [ "$verbosity" -ge 2 ] || return
 
@@ -18,17 +26,15 @@ debug() {
     sed "s|$HOME|~|g" >&2;
 }
 
-# TODO: call programs. add -v if verbosity -ge 1
-_mkdir() { echo "mkdir $*"; }
-_ln()    { echo "ln $*"; }
-_cp()    { echo "cp $*"; }
-
+# Return true if string $1 is present in the space-separated "array" $2.
 in_array() {
   local needle="$1" haystack="$2"
 
   printf " $haystack " | grep -Fq " $needle "
 }
 
+# Return true if the current source and base filename $1 match pattern
+# $2 where pattern is "(<source-glob>:)<filename-glob>"
 matches_pattern() {
   local file="$1" pattern="$2"
   local directory_part file_pattern
@@ -52,6 +58,8 @@ matches_pattern() {
   return 1
 }
 
+# Return true if base filename $1 matches any current exclusion
+# patterns.
 is_excluded() {
   local file="$1" pattern
 
@@ -65,6 +73,8 @@ is_excluded() {
   return 1
 }
 
+# Return true if base filename $1 matches any current inclusion
+# patterns.
 is_included() {
   local file="$1" pattern
 
@@ -78,6 +88,14 @@ is_included() {
   return 1
 }
 
+# Return true if the base filename $1 and $2 have the same contents.
+same_file() {
+  diff -q -s "$PWD/$1" "$2" > /dev/null
+}
+
+# Return true if base filename $1 should be skipped. This may be because
+# it doesn't exist, is a meta directory, or should be excluded based on
+# the known excludes list or (ex|in)clusion pattern checks.
 skip() {
   local file="$1"
 
@@ -106,6 +124,7 @@ skip() {
   return 1
 }
 
+# Print the installation location for base filename $1.
 destination() {
   local dotfile="$PWD/$1"
 
@@ -116,6 +135,8 @@ destination() {
   "
 }
 
+# Print 'X' or '@' if base filename $1 should be installed as a copy or
+# symlink respectively.
 sigil() {
   local file="$1"
 
@@ -130,6 +151,25 @@ sigil() {
   fi
 }
 
+# Installs $1 into $2.
+install_dotfile() {
+  local dotfile="$1" destination="$2"
+
+  debug "installing $dotfile as $destination"
+
+  [ -d "$directory" ] || _mkdir -p "$directory"
+
+  case "$(sigil "$dotfile")" in
+    '@') _ln -s "$dotfile" "$destination" ;;
+    'X') _cp "$dotfile" "$destination" ;;
+  esac
+}
+
+# Recursively enter directory $1 and call process_dotfile with each
+# relative filename within. Note that process_dotfile is not currently
+# defined and calls should do so before using this function. Also note
+# that it will be run in a subshell, changes to variables will not
+# persist and calls to exit will not terminate the script.
 dotfiles() {
   local directory="$1" file
 
@@ -150,6 +190,39 @@ dotfiles() {
 
     cd - >/dev/null
   )
+}
+
+# For each source, call dotfiles on it, then any host-specific sub
+# folder, then any tag-specific sub folders. TODO: run hooks.
+process_dotfiles() {
+  local hostname="${HOST:-$(hostname)}"
+  local dotfile host_dotfile tag_dotfile
+
+  for dotfiles in $dotfiles_dirs; do
+    debug "for source $dotfiles"
+
+    [ ! -d "$dotfiles" ] && continue
+
+    dotfiles "$dotfiles"
+
+    if [ "$include_host" -eq 1 ]; then
+      host_dotfiles="$dotfiles/host-$hostname"
+
+      if [ -d "$host_dotfiles" ]; then
+        debug "for host $hostname"
+        dotfiles "$host_dotfiles"
+      fi
+    fi
+
+    for tag in $tags; do
+      tag_dotfiles="$dotfiles/tag-$tag"
+
+      if [ -d "$tag_dotfiles" ]; then
+        debug "for tag $tag"
+        dotfiles "$tag_dotfiles"
+      fi
+    done
+  done
 }
 
 parse_options() {
@@ -175,52 +248,6 @@ parse_options() {
 
   files="$*"
 
-  dump_settings
-}
-
-process() {
-  local hostname="${HOSTNAME:-$(hostname)}"
-  local dotfile host_dotfile tag_dotfile
-
-  for dotfiles in $dotfiles_dirs; do
-    debug "for source $dotfiles"
-
-    if [ ! -d "$dotfiles" ]; then
-      debug "not found"
-      continue
-    fi
-
-    dotfiles "$dotfiles"
-
-    if [ "$include_host" -eq 1 ]; then
-      debug "for host $hostname"
-
-      host_dotfiles="$dotfiles/host-$hostname"
-
-      if [ -d "$host_dotfiles" ]; then
-        dotfiles "$host_dotfiles"
-      else
-        debug "not found"
-      fi
-    fi
-
-    if [ -n "$tags" ]; then
-      for tag in $tags; do
-        debug "for tag $tag"
-
-        tag_dotfiles="$dotfiles/tag-$tag"
-
-        if [ -d "$tag_dotfiles" ]; then
-          dotfiles "$tag_dotfiles"
-        else
-          debug "not found"
-        fi
-      done
-    fi
-  done
-}
-
-dump_settings() {
   debug "--- settings ---"
   debug "copy_all: $copy_all"
   debug "copy_always: $copy_always"
